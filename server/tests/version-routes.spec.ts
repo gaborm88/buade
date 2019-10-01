faec1cd58ca6f40cd0252e4ec3052b166e074368
Gabor Molnar <gabor.molnar@wunderdog.fi>
2019-10-01 10:15:50 +0300

    fix test

--- server/tests/version-routes.spec.ts
+++ server/tests/version-routes.spec.ts
@@ -1,16 +1,11 @@
 const request = require('supertest');
-const app = require("../src/local");
+const localapp = require("../src/local");
 
 describe("Smoke test", () => {
-  it("succeeds with correct credentials", async () => {
-    request(app)
-      .get('/user')
-      .expect('Content-Type', /json/)
-      .expect('Content-Length', '15')
-      .expect(200)
-      .end(function(err, res) {
-        if (err) throw err;
-      });
 
+  it("get a successful response", async () => {
+    const api = await request(localapp).get('/api')
+
+    expect(api.statusCode).toBe(200)
   });
 });
